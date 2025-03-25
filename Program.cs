7e6b85a83b2269e4f7643f474739c418d0afd748
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-25 19:38:40 -0300

    Atividade Cebolinha Feita

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+string cebolinha;
+
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.Write("Vou esperar você melhorar para poder provocar você");
+Console.ResetColor();
+Console.WriteLine();
+cebolinha = Console.ReadLine()! .Replace("r","l") .ToUpper();
+Console.WriteLine();
+Console.WriteLine($"// {cebolinha} //");
